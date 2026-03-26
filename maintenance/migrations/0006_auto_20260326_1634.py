01863060286517ee31e476babc7cb22b9b6e6dc9
anshggss <ptmtripathi@gmail.com>
2026-03-26 16:50:39 +0530

    wow

--- maintenance/migrations/0006_auto_20260326_1634.py
+++ maintenance/migrations/0006_auto_20260326_1634.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
                 migrations.AddField(
                     model_name='maintenancerecord',
                     name='parts_used',
-                    field=models.TextField(blank=True, default=''),
+                    field=models.JSONField(blank=True, default=list),
                 ),
             ],
             database_operations=[],  # Column already exists in the database
